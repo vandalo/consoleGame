a51d42b33318f2d594c8b36571f42de282f6a7a1
ferran.coma <ferran.coma@king.com>
2019-06-26 15:54:52 +0200

    adding all functionalitys to myVector

--- ConsoleGame/main/MyStd/MyAssert.cpp
+++ ConsoleGame/main/MyStd/MyAssert.cpp
@@ -3,6 +3,7 @@
 
 void MyAssert::assert(int errorNum, const char* text, const char* fileName, int fileLine)
 {
+	IOStream::cout << IOStream::endl;
 	IOStream::cout << "Error(" << errorNum << ") - " << text << IOStream::endl;
 	IOStream::cout << "File: " << fileName << " - Line:" << fileLine << IOStream::endl;
 	exit(0);
--- ConsoleGame/main/MyStd/MyVector.h
+++ ConsoleGame/main/MyStd/MyVector.h
@@ -11,13 +11,16 @@ public:
 
 	MyVector(int size, T defaultValue);
 	int size();
-	T* push(T newObject);
+	T* push_back(T newObject);
+	void pop_back();
 
 	T* begin();
 	T* end();
 	T& operator[](int pos);
 
 private:
+	void resize();
+
 	int m_realSize;
 	int m_size;
 	T* m_vector;
@@ -36,15 +39,17 @@ T& MyVector<T>::operator[](int pos)
 
 template <class T>
 MyVector<T>::MyVector() : 
-	m_realSize(64)
+	m_realSize(1)
 	, m_size(0)
 {
-	m_vector = new T [m_realSize];
+	m_vector = new T[m_realSize];
 }
 
 template <class T>
 MyVector<T>::~MyVector()
-{}
+{
+	delete[] m_vector;
+}
 
 template <class T>
 MyVector<T>::MyVector(int size, T defaultValue) :
@@ -61,11 +66,28 @@ int MyVector<T>::size()
 }
 
 template <class T>
-T* MyVector<T>::push(T newObject)
+T* MyVector<T>::push_back(T newObject)
 {
+	if (m_size >= m_realSize)
+	{
+		resize();
+	}
 	m_vector[m_size] = newObject;
 	++m_size;
 	return &m_vector[m_size];
+}
+
+template <class T>
+void MyVector<T>::pop_back()
+{
+	if (m_size > 0)
+	{
+		--m_size;
+	}
+	else
+	{
+		ASSERT(1, "MyVector was empty");
+	}
 }
 
 template <class T>
@@ -78,4 +100,17 @@ template <class T>
 T* MyVector<T>::end()
 {
 	return &m_vector[m_size];
+}
+
+template <class T>
+void MyVector<T>::resize()
+{
+	T* new_vector = new T[m_realSize * 2];
+	for (int i = 0; i < m_realSize; ++i)
+	{
+		new_vector[i] = m_vector[i];
+	}
+	delete[] m_vector;
+	m_realSize *= 2;
+	m_vector = new_vector;
 }
